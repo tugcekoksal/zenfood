4e534b2ef176f438837db719e28fde448d531162
tugcekoksal <tugcekoksal35@gmail.com>
2023-08-17 08:51:18 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container grid grid--5-cols">
-        <div className="footer-logo-col">
-          <a href="javascript:void(0)" className="footer-logo">
+        <div className="footer-logo">
             <img
               src={require("../img/zenfood-logo.png")}
               alt="footer logo"
@@ -20,7 +19,7 @@ function Footer() {
 
           <ul className="footer-icons">
             <li>
-              <a className="footer-link" href="javascript:void(0)">
+              <a className="footer-link" >
                 <PiFacebookLogoDuotone className="footer-icon" />
               </a>
             </li>
